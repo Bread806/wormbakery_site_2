8f91043432870252a8f08cc529e9db0ea75a353d
Bread <bread@Breads-MacBook-Air.local>
2026-01-06 17:43:57 +0800

    fix domain and signture

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  // When building for GitHub Pages we set GH_PAGES=1 in the build environment.
-  // Use that to set the base so assets are emitted with the correct subpath.
-  base: process.env.GH_PAGES ? '/wormbakery_site_2/' : '/',
+export default defineConfig(() => ({
+  base: process.env.CUSTOM_DOMAIN ? '/' : '/wormbakery_site_2/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+}));
